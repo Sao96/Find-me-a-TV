6340cca5a981f2dbc76a18489361b705521db9fa
Shadi <saothman@ucdavis.com>
2019-08-30 22:08:15 -0700

    Complete first version of processing a product page

--- scrape_walmart.py
+++ scrape_walmart.py
@@ -3,9 +3,9 @@ import requests
 import time
 from fake_useragent import UserAgent
 
-#Function that visits a desired web page
-#@url: url to visit
-#@return: the data sent back from the get request to @url
+#     Function that visits a desired web page
+#     @url: url to visit
+#     @return: the data sent back from the get request to @url
 def scrape_data(url: str) -> str:
       usr_agent=UserAgent()
 
@@ -25,26 +25,22 @@ def scrape_data(url: str) -> str:
       return (response.text).replace('\n','')
 
 
+#     returns a substring based on the first occurance of a beginning string and until a matched string
+#     @source: the string we are taking a substring of     @begin: the first match to start a substring from     @end:the string we will stop reading when encountering
+#     @return:  a substring ranging from first occurance of @begin up until the next occurance of @end, starting from @begin
 
-
-
-#returns a substring based on the first occurance of a beginning string and until a matched string
-#@source: the string we are taking a substring of     @begin: the first match to start a substring from     @end:the string we will stop reading when encountering
-#@return:  a substring ranging from first occurance of @begin up until the next occurance of @end, starting from @begin
 def substr_strs(source: str, begin: str, end: str) -> str:
       begin_index = source.find(begin)
-      end_index = (source[ begin_index: ]).find(end) + begin_index
-
+      end_index = source.find(end, begin_index) 
       print ('begin_index: ' + str(begin_index) + ', end_index: ' + str(end_index) )
+
       return source[ begin_index : end_index ] 
 
 
+#     Parses through a search results page and yanks out the links to each product
+#     @search_result: the html page recieved from the get request of a search
+#     @return: a list of strings, containing each product link
 
-
-
-#Parses through a search results page and yanks out the links to each product
-#@search_result: the html page recieved from the get request of a search
-#@return: a list of strings, containing each product link
 def parse_product_links(search_result: str) -> list:
       urls_list = list()
 
@@ -59,26 +55,68 @@ def parse_product_links(search_result: str) -> list:
       return urls_list
 
 
+#     takes a product page, reads it, and retruns a dict object of all metadata
+#     @prod_page: the current loaded product page
+#     @return: a dict objects containing the metadata of the given TV.
+
+def process_product_page(prod_page: str) -> dict:
+      tv_info = dict()
+      #trim the page to get to the part containing the meta data
+      prod_page = substr_strs(prod_page, '<td class="display-name" colspan="1" rowspan="1">Display Technology</td>', '<td class="display-name" colspan="1" rowspan="1">Assembled Product Dimensions (L x W x H)</td>' )
+      
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['disp_technology'] = data_range[len('<div>') : data_range.find('<\div>')]
+
+      prod_page = prod_page[ prod_page.find('<\div>') + len( '<\div>' ) : ]
+
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['resolution'] = data_range[len('<div>') : data_range.find('<\div>')]
+
+      #slice down to part with the model string
+      prod_page = prod_page [ prod_page.find('<td class="display-name" colspan="1" rowspan="1">Model</td>') : ]
+
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['model'] = data_range[len('<div>') : data_range.find('<\div>')]
+
+       #slice down to part with the screen size string
+      prod_page = prod_page [ prod_page.find('<td class="display-name" colspan="1" rowspan="1">Screen Size</td>') : ]
+
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['screen_size'] = data_range[len('<div>') : data_range.find('<\div>')]
+
+      #slice down to part with the brand name string
+      prod_page = prod_page [ prod_page.find('td class="display-name" colspan="1" rowspan="1">Brand</td>') : ]
+
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['brand'] = data_range[len('<div>') : data_range.find('<\div>')]
+
+      #slice down to part with the brand name string
+      prod_page = prod_page [ prod_page.find('<td class="display-name" colspan="1" rowspan="1">Refresh Rate</td>') : ]
+
+      data_range = substr_strs(prod_page,'<div>', '</div>')
+      tv_info['ref_rate'] = data_range[len('<div>') : data_range.find('<\div>')]
+      
+      return tv_info
 
 
+#     @urls_list: list of strings containing the url links to each product from search result page
+#     @return: a list of dict objects that contain all meta information about these TV's
 
-#@urls_list: list of strings containing the url links to each product from search result page
-#@return: a list of dict objects that contain all meta information about these TV's
-def process_urls(urls_list : list) -> list(){
+def process_urls(urls_list : list) -> list():
       products = list()
 
-      for prod_url in urls_list:
+      #for prod_url in urls_list:
             #sleep for 10 (will be 30) seconds to gracefully scrape with very minimal requests. MUST update time when done, too agressive now.
             time.sleep(10)
-            curr_page = scrape_data("https://walmart.com" + prod_url)
+           # curr_page = scrape_data("https://walmart.com" + prod_url)
+           #need to replace this with above line
+      with open('prodpage.html', 'r') as myfile:
+            curr_page = myfile.read()
 
-}
+      
+#     main routine to scrape each TV off walmart website
+#     @return: a list of dict objects containing meta info about every
 
-
-
-
-#main routine to scrape each TV off walmart website
-#@return: a list of dict objects containing meta info about every
 def scrape_walmart_tvs() -> list:
       web_name = 'https://walmart.com'
 
