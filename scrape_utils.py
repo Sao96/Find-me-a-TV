a110297c95aa1300bd54126d49e90c73af6533b4
Shadi <saothman@ucdavis.com>
2019-08-31 15:07:54 -0700

    separate standard scrape utilities from the walmart scraper

---
+++ scrape_utils.py
@@ -0,0 +1,33 @@
+import requests
+from fake_useragent import UserAgent
+
+#     Function that visits a desired web page
+#     @url: url to visit
+#     @return: the data sent back from the get request to @url
+def scrape_data(url: str) -> str:
+
+      usr_agent=UserAgent()
+
+      #make a pretty looking real header set
+      header_dict = {'User-Agent': usr_agent.random,
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3,application/json,text/plain',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
+
+      response = requests.get(url, headers=header_dict)
+
+      #clean out all of the generated \n characters
+      return (response.text).replace('\n','')
+
+
+#     returns a substring based on the first occurance of a beginning string and until a matched string
+#     @source: the string we are taking a substring of     @begin: the first match to start a substring from     @end:the string we will stop reading when encountering
+#     @return:  a substring ranging from first occurance of @begin up until the next occurance of @end, starting from @begin
+
+def substr_strs(source: str, begin: str, end: str) -> str:
+      begin_index = source.find(begin)
+      end_index = source.find(end, begin_index) 
+
+      return source[ begin_index : end_index + 1 ] 
