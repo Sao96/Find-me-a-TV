54dea9bc89238b3cfcd27abe6b46f6b199bc808f
Shadi <saothman@ucdavis.com>
2019-09-01 13:11:14 -0700

    continue development of scrape amazon

--- scrape_amazon.py
+++ scrape_amazon.py
@@ -30,7 +30,60 @@ def parse_product_links(search_result: str) -> list:
     
     return filter(remove_bad_url, urls_list)
 
+def process_product_page(prod_page: str) -> dict:
+    tv_info = dict()
+    prod_page = substr_strs(prod_page, '<span class="a-size-base a-color-base">Customer Rating</span>', 'Total HDMI Ports')
+    
+    tv_info['price'] = substr_strs(prod_page, '$', '<')
 
+    loop_targets = ['Display Size', 'Display Type', 'disp_tech', 'Refresh Rate', 'Resolution' ]
+
+    #push prod_page to next important column of info table
+    prod_page = prod_page[ prod_page.find('Display Size') : ]
+    prod_page = prod_page[ prod_page.find('<span class="a-size-base a-color-base">') : ]
+
+    
+
+    tv_info['screen_size'] = substr_strs(prod_page, '>', '<') 
+
+    #push prod_page to next important column of info table
+    prod_page = prod_page[ prod_page.find('Display Type') : ]
+    prod_page = prod_page[ prod_page.find('<span class="a-size-base a-color-base">') : ]
+
+    tv_info['disp_technology'] = substr_strs(prod_page, '>', '<') 
+
+    #push prod_page to next important column of info table
+    prod_page = prod_page[ prod_page.find('Refresh Rate') : ]
+    prod_page = prod_page[ prod_page.find('<span class="a-size-base a-color-base">') : ]
+
+    tv_info['ref_rate'] = substr_strs(prod_page, '>', '<') 
+
+    #push prod_page to next important column of info table
+    prod_page = prod_page[ prod_page.find('Resolution') : ]
+    prod_page = prod_page[ prod_page.find('<span class="a-size-base a-color-base">') : ]
+
+    tv_info['resolution'] = substr_strs(prod_page, '>', '<') 
+
+
+
+def process_urls(urls_list : list) -> list():
+    products = list()
+
+    ctr = 0
+
+    #   for prod_url in urls_list:
+    #         #sleep for 10 (will be 30) seconds to gracefully scrape with very minimal requests. MUST update time when done, too agressive now.
+    #         time.sleep(2)
+    #         curr_page = scrape_data("https://amazon.com" + prod_url)
+    #         products.append(process_product_page(curr_page))
+
+    with open('amazon_prod.html', 'r') as myFile:
+        curr_page = myFile.read()      
+        
+
+    process_product_page(curr_page)
+
+    #return products
 
 def scrape_amazon_tvs() -> list:
     web_name = 'https://amazon.com'
@@ -46,6 +99,6 @@ def scrape_amazon_tvs() -> list:
     
     urls_list = parse_product_links(curr_page)
 
-    return urls_list
+    return process_urls(urls_list)
 
 
